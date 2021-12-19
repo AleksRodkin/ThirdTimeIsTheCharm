fa90844e9807ce446a16f9a281a9d1e6cb93ab07
Aleks Rodkin <aleksrodkin95@gmail.com>
2021-12-19 20:17:24 +0300

    GeekBrains Swift Interface Lesson 1 HomeWork

--- ThirdTimeIsTheCharm/Views/LoginViewController.swift
+++ ThirdTimeIsTheCharm/Views/LoginViewController.swift
@@ -15,8 +15,6 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordLabel: UILabel!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBAction func loginButton(_ sender: Any) {
-        print(userNameTextField.text)
-        print(passwordTextField.text)
     
     }
     
